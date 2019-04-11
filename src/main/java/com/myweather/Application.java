0cb4154d3235d719c94d036900e3480c5764d1dc
c58139a <derek.helbert@experian.com>
2019-04-11 16:15:50 -0700

    Added html page

--- src/main/java/com/myweather/Application.java
+++ src/main/java/com/myweather/Application.java
@@ -1,7 +1,13 @@
 package com.myweather;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
-        System.out.println("My Weather!");
+        SpringApplication.run(Application.class, args);
     }
+
 }
